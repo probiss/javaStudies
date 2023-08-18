5c33e3bfcda30dedf1dc67c92364f544988ff5d9
probiss <bulut.cihad@gmail.com>
2023-08-18 16:43:08 +0300

    mini2 done.

--- src/com/enginDemirog/miniProjects/Mini2.java
+++ src/com/enginDemirog/miniProjects/Mini2.java
@@ -2,6 +2,20 @@ package com.enginDemirog.miniProjects;
 
 public class Mini2 {
     public static void main(String[] args) {
+        char letter = 'i';
 
+        switch (letter) {
+            case 'A':
+            case 'I':
+            case 'O':
+            case 'U':
+            case 'a':
+            case 'o':
+            case 'u':
+                System.out.println("Bold vowel.");
+                break;
+            default:
+                System.out.println("Not bold vowel.");
+        }
     }
 }
